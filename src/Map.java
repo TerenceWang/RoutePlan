1adc0b700f0d1067d043611a876ddc6fcf68e691
Terence <wangtaowxs@gmail.com>
2016-10-01 14:29:58 +0200

    add readmap function with adjacency matrix

---
+++ src/Map.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * Created by terence on 10/1/16.
+ */
+public class Map {
+
+    public AMWGraph graph;
+    public int nodeperline;
+    public int nodetotal;
+    public int edgetotal;
+    Map(){
+
+    }
+
+    public void readmap(String filename){
+        File file = new File(filename);
+        BufferedReader reader=null;
+        try{
+            reader = new BufferedReader(new FileReader(file));
+            String tmp=reader.readLine();
+            String[] parameters=tmp.split(" ");
+            nodeperline=Integer.parseInt(parameters[0]);
+            nodetotal=Integer.parseInt(parameters[1]);
+            edgetotal=Integer.parseInt(parameters[2]);
+            graph= new AMWGraph(nodetotal);
+            for (int i = 0; i < nodetotal; i++) {
+                graph.insertVertex(i);
+            }
+            while((tmp = reader.readLine())!=null){
+                parameters=tmp.split(" ");
+                graph.insertEdge(Integer.parseInt(parameters[0]),Integer.parseInt(parameters[1])
+                        ,Integer.parseInt(parameters[2]));
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }finally{
+            if(reader!=null){
+                try {
+                    reader.close();
+                }catch (IOException e1){
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Map m=new Map();
+        m.readmap("data/nodenumber_10_edgenumber_327.map");
+        System.out.println(m.graph.getFirstNeighbor(5));
+        System.out.println(m.nodeperline+"\t"+m.edgetotal);
+    }
+}
